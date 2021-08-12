e0027145c154c3db2876674a658348c7ade048c9
Alexis Quintuna <73076646+alexisquintuna@users.noreply.github.com>
2021-08-12 10:54:40 -0500

    Delete djangostock directory

--- djangostock/stockapp/stockapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from quote import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('quote.urls')),
-]
